bff796144d223f12d2954989469ba959e2e6df69
hellion86 <vmezhurevsky@gmail.com>
2018-04-22 22:36:22 +0700

    complete lesson_2

---
+++ lesson_2/task5.py
@@ -0,0 +1,15 @@
+'''Условие
+Даны три целых числа. Выведите значение наименьшего из них.
+'''
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+if a <= b and a <= c:
+    print(str(a))
+elif b <= a and b <= c:
+    print(str(b))
+else:
+    print(str(c))
+
