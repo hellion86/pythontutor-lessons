93d78058eb75ddd69b863942d1721adb7ca26043
hellion86 <vmezhurevsky@gmail.com>
2018-05-12 00:20:48 +0700

    lesson_6 working

--- lesson_6/Колличество элементов равных максимуму.py
+++ lesson_6/Колличество элементов равных максимуму.py
@@ -5,6 +5,26 @@
 '''
 
 # Мое решение 
- 
+s = 1
+s1 = 1
+count = 0
+while s != 0:
+    s = int(input())
+    if s1 <= s:
+        if s1 != s:
+            count = 0
+        s1 = s
+        count = count + 1
+print(count)
+
 # Правильное решение
- 
+maximum = 0
+num_maximal = 0
+element = -1
+while element != 0:
+    element = int(input())
+    if element > maximum:
+        maximum, num_maximal = element, 1
+    elif element == maximum:
+        num_maximal += 1        
+print(num_maximal)
