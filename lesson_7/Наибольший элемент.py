80a50f971d26ecedb76797b0c5454106e29fe4c3
hellion86 <vmezhurevsky@gmail.com>
2018-06-14 23:23:22 +0700

    Lesson_7 working

---
+++ lesson_7/Наибольший элемент.py
@@ -0,0 +1,17 @@
+'''
+ Условие
+Дан список чисел. Выведите значение наибольшего элемента в списке, а затем индекс этого элемента в списке. Если наибольших элементов несколько, выведите индекс первого из них. 
+'''
+
+# Мое решение 
+a = [int(s) for s in input().split()]
+s = max(a)
+print(s,a.index(s))
+
+# Правильное решение
+index_of_max = 0
+a = [int(i) for i in input().split()]
+for i in range(1, len(a)):
+    if a[i] > a[index_of_max]:
+        index_of_max = i
+print(a[index_of_max], index_of_max)
